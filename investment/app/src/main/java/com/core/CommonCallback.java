680660eba8affa96a5a5dbbdc82a5c6acc95bb91
bin.teng <tengbinlive@gmail.com>
2015-06-24 10:14:44 +0800

    test

--- investment/app/src/main/java/com/core/CommonCallback.java
+++ investment/app/src/main/java/com/core/CommonCallback.java
@@ -2,18 +2,24 @@ package com.core;
 
 /**
  * 通用回调接口.
- * 
+ *
  * @author bin.teng
  */
 public interface CommonCallback {
 
-	/** 开始操作 */
-	public void begin();
+    /**
+     * 开始操作
+     */
+    public void begin();
 
-	/** 结束操作 */
-	public void end();
+    /**
+     * 结束操作
+     */
+    public void end();
 
-	/** 数据回调 */
-	public void resp(CommonResponse response);
+    /**
+     * 数据回调
+     */
+    public void resp(CommonResponse response);
 
 }
