fca7dbb9f85d41236ab4835853b2794100f27e00
bin.teng <tengbinlive@gmail.com>
2015-07-23 10:32:41 +0800

    no message

---
+++ investment/app/src/main/java/com/touyan/investment/manager/MessageManager.java
@@ -0,0 +1,12 @@
+package com.touyan.investment.manager;
+
+/**
+ * 通信&消息相关业务类.
+ *
+ * @author HuangChao
+ */
+public class MessageManager {
+
+    private static final String TAG = MessageManager.class.getSimpleName();
+
+}
