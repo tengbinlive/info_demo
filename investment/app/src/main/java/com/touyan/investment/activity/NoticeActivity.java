f8ab17e845bd1d9836deeeb01aa4a9e855012bf1
bin.teng <tengbinlive@gmail.com>
2015-07-29 16:30:40 +0800

    no message

--- investment/app/src/main/java/com/touyan/investment/activity/NoticeActivity.java
+++ investment/app/src/main/java/com/touyan/investment/activity/NoticeActivity.java
@@ -11,7 +11,10 @@ import com.nhaarman.listviewanimations.appearance.simple.SwingBottomInAnimationA
 import com.touyan.investment.AbsActivity;
 import com.touyan.investment.R;
 import com.touyan.investment.adapter.NoticeAdapter;
+import com.touyan.investment.bean.message.GroupDetail;
 import com.touyan.investment.bean.message.InviteMessage;
+import com.touyan.investment.bean.user.BatchInfoResult;
+import com.touyan.investment.bean.user.UserInfo;
 import com.touyan.investment.hx.HXCacheUtils;
 import com.touyan.investment.manager.UserManager;
 
@@ -24,8 +27,6 @@ public class NoticeActivity extends AbsActivity {
     ListView listview;
     @Bind(R.id.ll_listEmpty)
     LinearLayout llListEmpty;
-
-    private ArrayList<InviteMessage> messageArrayList;
 
     private HashMap<String, InviteMessage> messageHashMap;
 
@@ -53,6 +54,23 @@ public class NoticeActivity extends AbsActivity {
      */
     private void loadData(CommonResponse resposne) {
         if (resposne.isSuccess()) {
+            BatchInfoResult result = (BatchInfoResult) resposne.getData();
+            ArrayList<UserInfo> userinfo = result.getUserinfo();
+            ArrayList<GroupDetail> groupinfo = result.getGroupinfo();
+            if (null != userinfo) {
+                for (UserInfo userInfo : userinfo) {
+                    messageHashMap.get(userInfo.getServno()).setFrom(userInfo.getUalias());
+                    messageHashMap.get(userInfo.getServno()).setHeadphoto(userInfo.getUphoto());
+                }
+            }
+            if (null != groupinfo) {
+                for (GroupDetail groupDetail : groupinfo) {
+                    messageHashMap.get(groupDetail.getGroupid()).setGroupName(groupDetail.getGroupname());
+                    messageHashMap.get(groupDetail.getGroupid()).setHeadphoto(groupDetail.getGphoto());
+                    messageHashMap.get(groupDetail.getGroupid()).setGroupId(groupDetail.getGroupid());
+                }
+            }
+            mAdapter.refresh(new ArrayList<>(HXCacheUtils.getInstance().getInviteMessageHashMap().values()));
         } else {
             CommonUtil.showToast(resposne.getErrorTip());
         }
@@ -62,9 +80,9 @@ public class NoticeActivity extends AbsActivity {
     @Override
     public void EInit() {
         super.EInit();
-        initListView();
         messageHashMap = HXCacheUtils.getInstance().getInviteMessageHashMap();
-        messageArrayList = new ArrayList<>(HXCacheUtils.getInstance().getInviteMessageHashMap().values());
+        initListView();
+        getDataList();
     }
 
     @Override
@@ -80,7 +98,7 @@ public class NoticeActivity extends AbsActivity {
 
     private void initListView() {
 
-        mAdapter = new NoticeAdapter(this, null);
+        mAdapter = new NoticeAdapter(this, new ArrayList<>(HXCacheUtils.getInstance().getInviteMessageHashMap().values()));
 
         SwingBottomInAnimationAdapter animationAdapter = new SwingBottomInAnimationAdapter(mAdapter);
 
@@ -92,12 +110,11 @@ public class NoticeActivity extends AbsActivity {
     }
 
     private void getDataList() {
-//        ArrayList<String> forms = new ArrayList<>(HXCacheUtils.getInstance().getFriendsHashMap().keySet());
-//        ArrayList<String> users = new ArrayList<>();
-//        if (usernames != null) {
-//            UserManager userManager = new UserManager();
-//            userManager.batchInfo(NoticeActivity.this, usernames, new ArrayList<String>(), activityHandler, LOAD_DATA);
-//        }
+        ArrayList<String> forms = new ArrayList<>(HXCacheUtils.getInstance().getInviteMessageHashMap().keySet());
+        if (forms != null) {
+            UserManager userManager = new UserManager();
+            userManager.batchInfo(NoticeActivity.this, forms, new ArrayList<String>(), activityHandler, LOAD_DATA);
+        }
     }
 
 }
--- investment/app/src/main/java/com/touyan/investment/adapter/NoticeAdapter.java
+++ investment/app/src/main/java/com/touyan/investment/adapter/NoticeAdapter.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.core.CommonResponse;
 import com.core.util.CommonUtil;
 import com.core.util.DateUtil;
+import com.core.util.StringUtil;
 import com.joooonho.SelectableRoundedImageView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.touyan.investment.R;
@@ -84,37 +86,82 @@ public class NoticeAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
         if (convertView == null) {
-            convertView = mInflater.inflate(R.layout.item_add_friend, null);
+            convertView = mInflater.inflate(R.layout.item_notice, null);
             holder = new ViewHolder(convertView);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
-//        InviteMessage message = list.get(position);
-//        ImageLoader.getInstance().displayImage(userInfo.getUphoto(), holder.head);
-//        holder.name.setText(userInfo.getUalias());
-//        String dateStr = DateUtil.ConverToString(infoBean.getPubstm(), DateUtil.YYYY_MM_DD_HH_MM_SS);
-
-
+        InviteMessage message = list.get(position);
+        ImageLoader.getInstance().displayImage(message.getHeadphoto(), holder.head);
+        String groupName = message.getGroupName();
+        String name = StringUtil.isNotBlank(groupName) ? groupName : message.getFrom();
+        holder.name.setText(name);
+        String dateStr = DateUtil.getDateString(message.getTime(), DateUtil.YYYY_MM_DD_HH_MM_SS);
+        holder.date.setText(dateStr);
+        InviteMessage.InviteMesageStatus status = message.getStatus();
+        if (status == InviteMessage.InviteMesageStatus.BEINVITEED) {
+            setStatusButton(holder, "接收", "拒绝", false);
+        } else if (status == InviteMessage.InviteMesageStatus.BEAGREED) {
+            setStatusButton(holder, "对方同意", null, true);
+        } else if (status == InviteMessage.InviteMesageStatus.BEREFUSED) {
+            setStatusButton(holder, "被拒绝", null, true);
+        } else if (status == InviteMessage.InviteMesageStatus.AGREED) {
+            setStatusButton(holder, "已同意", null, true);
+        } else if (status == InviteMessage.InviteMesageStatus.REFUSED) {
+            setStatusButton(holder, "已拒绝", null, true);
+        } else if (status == InviteMessage.InviteMesageStatus.BEAPPLYED) {
+            setStatusButton(holder, "同意", "拒绝", false);
+        }
         return convertView;
+    }
+
+    /**
+     * 设置状态按钮
+     *
+     * @param holder
+     * @param yesStr
+     * @param noStr
+     * @param isCarry true表示完成状态，false表示按钮状态
+     */
+    private void setStatusButton(ViewHolder holder, String yesStr, String noStr, boolean isCarry) {
+        holder.statusYes.setText(yesStr);
+        if (StringUtil.isNotBlank(noStr)) {
+            holder.statusNo.setVisibility(View.VISIBLE);
+            holder.statusNo.setText(noStr);
+        } else {
+            holder.statusNo.setVisibility(View.GONE);
+        }
+        if (!isCarry) {
+            holder.statusYes.setBackgroundResource(R.drawable.bg_round_red);
+            holder.statusYes.setEnabled(true);
+        } else {
+            holder.statusYes.setEnabled(false);
+            holder.statusYes.setBackground(null);
+        }
     }
 
 
     /**
-     * This class contains all butterknife-injected Views & Layouts from layout file 'item_add_friend.xml'
+     * This class contains all butterknife-injected Views & Layouts from layout file 'item_notice.xml'
      * for easy to all layout elements.
      *
      * @author ButterKnifeZelezny, plugin for Android Studio by Avast Developers (http://github.com/avast)
      */
-
-    class ViewHolder {
+    static class ViewHolder {
         @Bind(R.id.head)
         SelectableRoundedImageView head;
-        @Bind(R.id.add)
-        TextView add;
+        @Bind(R.id.status_yes)
+        TextView statusYes;
+        @Bind(R.id.status_no)
+        TextView statusNo;
+        @Bind(R.id.status_layout)
+        LinearLayout statusLayout;
         @Bind(R.id.name)
         TextView name;
+        @Bind(R.id.date)
+        TextView date;
 
         ViewHolder(View view) {
             ButterKnife.bind(this, view);
