aa04d3eb2569271bfd03f5933bddb23a406bf003
tengbin <tengbin@boqii.com>
2016-03-14 15:20:38 +0800

    无帐号demo 直接登录

--- investment/app/src/main/java/com/touyan/investment/activity/AboutInvActivity.java
+++ investment/app/src/main/java/com/touyan/investment/activity/AboutInvActivity.java
@@ -1,10 +1,19 @@
 package com.touyan.investment.activity;
 
+import android.widget.TextView;
+
 import com.touyan.investment.AbsActivity;
+import com.touyan.investment.BuildConfig;
 import com.touyan.investment.R;
 
 public class AboutInvActivity extends AbsActivity {
 
+    @Override
+    public void EInit() {
+        super.EInit();
+        TextView version = (TextView) findViewById(R.id.version_tv);
+        version.setText(getString(R.string.app_name)+" v"+ BuildConfig.VERSION_NAME);
+    }
 
     @Override
     public int getContentView() {
